a984ae725e8f660fbb1c6520827828839662da1e
Irshad <irshad.kuttipallerakath@gyansys.com>
2024-09-09 14:59:21 +0530

    Initial commit

---
+++ backend/src/test/java/com/spring/electronics/ElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
