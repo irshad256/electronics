42a78b48ea380284e0bd948d3c5db5d157670eb9
Irshad <irshad.kuttipallerakath@gyansys.com>
2024-09-09 15:47:48 +0530

    Created user and role entity

---
+++ backend/src/main/java/com/spring/electronics/role/Role.java
@@ -0,0 +1,43 @@
+package com.spring.electronics.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.spring.electronics.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
