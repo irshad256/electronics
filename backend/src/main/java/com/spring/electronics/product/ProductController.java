c3979866c97dbf9d8213088ac99cfef586e76ddc
Irshad <irshad.kuttipallerakath@gyansys.com>
2024-10-04 19:05:40 +0530

    Product Details Page

--- backend/src/main/java/com/spring/electronics/product/ProductController.java
+++ backend/src/main/java/com/spring/electronics/product/ProductController.java
@@ -1,15 +1,13 @@
 package com.spring.electronics.product;
 
 
+import com.spring.electronics.mapper.ProductMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,12 +20,17 @@ public class ProductController {
 
     private final ProductRepository productRepository;
 
-    private final ProductService productService;
+    private final ProductMapper productMapper;
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Set<ProductDto>> getAllProduct() {
         Set<ProductDto> products = productRepository.findAll().stream().map(Product::getProductDto).collect(Collectors.toSet());
         return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ProductDto getProductForCode(@PathVariable String code){
+        return productMapper.productToProductDto(productRepository.findByCode(code).orElseThrow(()->new RuntimeException("Product Not found")));
     }
 
 }
